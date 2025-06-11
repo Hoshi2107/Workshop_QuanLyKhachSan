9c4349b488d75d848735071fc221d5b558f90686
DRX-Lazyfeel <baohpgtb01633@gmail.com>
2025-06-12 00:19:49 +0700

    tai lieu du an xuong thieu trang thai dat phong

---
+++ BLL_QuanLyKhachSan/BLLTrangThaiDatPhong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_QuanLyKhachSan;
+using DTO_QuanLyKhachSan;
+
+
+namespace BLL_QuanLyKhachSan
+{
+    public class BLLTrangThaiDatPhong
+    {
+      
+    }
+}
---
+++ DAL_QuanLyKhachSan/DALTrangThaiDatPhong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_QuanLyKhachSan;
+using Microsoft.Data.SqlClient;
+
+namespace DAL_QuanLyKhachSan
+{
+    public class DALTrangThaiDatPhong
+    {
+       
+    }
+}
+    
