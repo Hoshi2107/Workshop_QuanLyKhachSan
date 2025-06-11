6328cadd58295fe6b825ab6a7dd5ebf273a37f4c
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2025-06-11 11:43:49 +0700

    huy đang làm loại trạng thái đặt phòng (chưa xong)

--- GUI_QuanLyKhachSan/LoaiTrangThaiDatphong.cs
+++ GUI_QuanLyKhachSan/LoaiTrangThaiDatphong.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL_QuanLyKhachSan;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +18,26 @@ namespace GUI_QuanLyKhachSan
             InitializeComponent();
         }
 
+        private void LoadLoaiTrangThaiDatPhong()
+        {
+            BLL_NhanVien bUSNhanVien = new BLL_NhanVien();
+            guna2DgvNhanVien.DataSource = null;
+            guna2DgvNhanVien.DataSource = bUSNhanVien.GetNhanViensList();
+            guna2DgvNhanVien.Columns["MaNV"].HeaderText = "Mã Nhân Viên";
+            guna2DgvNhanVien.Columns["HoTen"].HeaderText = "Họ Và Tên";
+            guna2DgvNhanVien.Columns["GioiTinh"].HeaderText = "Gioi Tinh";
+            guna2DgvNhanVien.Columns["Email"].HeaderText = "Email";
+            guna2DgvNhanVien.Columns["DiaChi"].HeaderText = "Địa chỉ";
+            guna2DgvNhanVien.Columns["MatKhau"].HeaderText = "Mật khẩu";
+            guna2DgvNhanVien.Columns["VaiTroText"].HeaderText = "Vai Trò";
+            guna2DgvNhanVien.Columns["TinhTrangText"].HeaderText = "Tình Trạng";
+            guna2DgvNhanVien.Columns["vaiTro"].Visible = false;
+            guna2DgvNhanVien.Columns["TinhTrang"].Visible = false;
+            guna2DgvNhanVien.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
         private void LoaiTrangThaiDatphong_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
