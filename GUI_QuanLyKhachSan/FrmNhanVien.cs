8dc8521afadd63219db5c431e3b82d080d74c99d
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2025-07-23 08:54:03 +0700

    nhanvien

--- GUI_QuanLyKhachSan/FrmNhanVien.cs
+++ GUI_QuanLyKhachSan/FrmNhanVien.cs
@@ -141,146 +141,6 @@ namespace GUI_QuanLyKhachSan
             ClearForm();
         }
 
-        //private void btnsua_Click(object sender, EventArgs e)
-        //{
-
-
-        //    string hoTen = txthoten.Text.Trim();
-        //    string gioiTinh = txtgioitinh.Text.Trim();
-        //    string email = txtemail.Text.Trim();
-        //    string diaChi = txtdiachi.Text.Trim();
-        //    string matKhau = txtmatkhau.Text.Trim();
-        //    bool vaiTro;
-        //    if (rdoquanly.Checked)
-        //    {
-        //        vaiTro = true; // Quản lý
-        //    }
-        //    else if (rdonhanvien.Checked)
-        //    {
-        //        vaiTro = false; // Nhân viên
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Vui lòng chọn vai trò cho nhân viên.");
-        //        return;
-        //    }
-        //    bool tinhTrang;
-        //    if (rdoconhoatdong.Checked)
-        //    {
-        //        tinhTrang = true; // Hoạt động
-        //    }
-        //    else if (rdongunghoatdong.Checked)
-        //    {
-        //        tinhTrang = false; // Ngừng hoạt động
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Vui lòng chọn tình trạng cho nhân viên.");
-        //        return;
-        //    }
-        //    if (string.IsNullOrEmpty(hoTen) || string.IsNullOrEmpty(gioiTinh) ||
-        //        string.IsNullOrEmpty(email) || string.IsNullOrEmpty(diaChi) || string.IsNullOrEmpty(matKhau))
-        //    {
-        //        MessageBox.Show("Vui lòng điền đầy đủ thông tin nhân viên.");
-        //        return;
-        //    }
-        //    DTO_NhanVien nv = new DTO_NhanVien
-        //    {
-
-        //        HoTen = hoTen,
-        //        GioiTinh = gioiTinh,
-        //        Email = email,
-        //        DiaChi = diaChi,
-        //        MatKhau = matKhau,
-        //        VaiTro = vaiTro,
-        //        TinhTrang = tinhTrang
-        //    };
-        //    BLL_NhanVien bUSNhanVien = new BLL_NhanVien();
-        //    string result = bUSNhanVien.UpdateNhanVien(nv);
-        //    if (string.IsNullOrEmpty(result))
-        //    {
-        //        MessageBox.Show("Cập nhật nhân viên thành công.");
-        //        loadDanhSachNhanVien();
-        //        ClearForm();
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show(result);
-        //    }
-
-        //    string hoTen = txthoten.Text.Trim();
-        //    string gioiTinh = txtgioitinh.Text.Trim();
-        //    string email = txtemail.Text.Trim();
-        //    string diaChi = txtdiachi.Text.Trim();
-        //    string matKhau = txtmatkhau.Text.Trim();
-
-        //    bool tinhTrang;
-        //    if (rdoconhoatdong.Checked)
-        //    {
-        //        tinhTrang = true; // Hoạt động
-        //    }
-        //    else if (rdongunghoatdong.Checked)
-        //    {
-        //        tinhTrang = false; // Ngừng hoạt động
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Vui lòng chọn tình trạng cho nhân viên.");
-        //        return;
-        //    }
-        //    bool vaiTro;
-        //    if (rdoquanly.Checked)
-        //    {
-        //        vaiTro = true; // Quản lý
-        //    }
-        //    else if (rdonhanvien.Checked)
-        //    {
-        //        vaiTro = false; // Nhân viên
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Vui lòng chọn vai trò cho nhân viên.");
-        //        return;
-        //    }
-        //    if (string.IsNullOrEmpty(hoTen) || string.IsNullOrEmpty(gioiTinh) ||
-        //        string.IsNullOrEmpty(email) || string.IsNullOrEmpty(diaChi) || string.IsNullOrEmpty(matKhau))
-        //    {
-        //        MessageBox.Show("Vui lòng điền đầy đủ thông tin nhân viên.");
-        //        return;
-        //    }
-
-        //    DTO_NhanVien nv = new DTO_NhanVien
-        //    {
-        //        // Tự động sinh mã nhân viên
-        //        HoTen = hoTen,
-        //        GioiTinh = gioiTinh,
-        //        Email = email,
-        //        DiaChi = diaChi,
-        //        MatKhau = matKhau,
-        //        TinhTrang = tinhTrang,
-        //        VaiTro = vaiTro
-
-        //    };
-        //    BLL_NhanVien bUSNhanVien = new BLL_NhanVien();
-        //    string result = bUSNhanVien.InsertNhanVien(nv);
-        //    if (string.IsNullOrEmpty(result))
-        //    {
-        //        MessageBox.Show("Thêm nhân viên thành công.");
-        //        loadDanhSachNhanVien();
-        //        ClearForm();
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show(result);
-        //    }
-
-        //}
-
-        //private void btnlammoi_Click(object sender, EventArgs e)
-        //{
-        //    ClearForm();
-        //}
-
         private void btnsua_Click(object sender, EventArgs e)
         {
             string hoTen = txthoten.Text.Trim();
@@ -398,7 +258,26 @@ namespace GUI_QuanLyKhachSan
 
         private void btnxoa_Click(object sender, EventArgs e)
         {
+            if (guna2DgvNhanVien.SelectedRows.Count > 0)
+            {
+                string maNV = guna2DgvNhanVien.SelectedRows[0].Cells["MaNV"].Value.ToString();
+                BLL_NhanVien bllNhanVien = new BLL_NhanVien();
+                string result = bllNhanVien.DeleteNhanVien(new DTO_NhanVien { MaNV = maNV });
 
+                if (string.IsNullOrEmpty(result))
+                {
+                    MessageBox.Show("Xóa đặt phòng thành công!");
+                    loadDanhSachNhanVien();
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi khi xóa: " + result);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn dòng cần xóa!");
+            }
         }
 
         private void guna2Button4_Click(object sender, EventArgs e)
